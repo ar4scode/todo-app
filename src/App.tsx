b19eef77862db41df960589adb2dea5a84c2a9f8
Ar4sCode <ar4sazizi@gmail.com>
2025-08-30 19:53:59 +0330

    Create a container for to-do list

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
+import { useState } from 'react';
 import './App.css';
 import ButtonsGroup from './components/ButtonsGroup';
-import { Button } from '@mui/material';
+import IconButton from "@mui/material/IconButton"; 
+import DeleteIcon from "@mui/icons-material/Delete"; 
+import Checkbox from "@mui/material/Checkbox"; 
 
 function App() {
-
   return (
-    <div className='flex flex-col items-center'>
-      <Button
-        variant='contained'
-        sx={{borderRadius: "100%", padding: "14px", fontSize: "20px", backgroundColor:"purple", width: "40px"}}
-      >
-        +
-      </Button>
+    <div className='flex flex-col items-center mb-70'>
       <ButtonsGroup />
+
+      {/* To Do list */}
+      <div>
+        <ul className='mt-10'>
+          <li className='flex justify-between items-center w-70 text-purple-800 font-bold border-1 p-2 rounded-md shadow-md text-sm'>
+            <div>
+              <Checkbox  color="success" />
+              do my job
+            </div>
+            <IconButton aria-label="delete" size="small">
+              <DeleteIcon fontSize="small" sx={{ color: "purple" }} />
+            </IconButton>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
