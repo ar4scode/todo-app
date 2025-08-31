269d21c66755c9827c11f9d0e7216351d56fcd36
Ar4sCode <ar4sazizi@gmail.com>
2025-08-31 11:22:39 +0330

    Add input that get value from user and show it on screen

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,68 @@ import ButtonsGroup from './components/ButtonsGroup';
 import IconButton from "@mui/material/IconButton"; 
 import DeleteIcon from "@mui/icons-material/Delete"; 
 import Checkbox from "@mui/material/Checkbox"; 
+import { TextField, Button } from '@mui/material';
+import { nanoid } from 'nanoid';
 
 function App() {
+  interface Todo {
+    id: string;
+    inputValue: string;
+  }
+
+  // input should be a string
+  const [inputValue, setInputValue] = useState<string>("");
+
+  // todoList should be an array of Todo objects
+  const [todoList, setTodoList] = useState<Todo[]>([]);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleAddToTodo = () => {
+    if (inputValue.trim() === "") return; // avoid empty todos
+    setTodoList((prev) => [
+      ...prev,
+      {
+        id: nanoid(),
+        inputValue: inputValue,
+      },
+    ]);
+    setInputValue(""); // clear input after adding
+  };
+
   return (
     <div className='flex flex-col items-center mb-70'>
+      <form className='flex' onSubmit={(e) => e.preventDefault()}>
+        <TextField 
+          id="todo-value" 
+          label="To-Do" 
+          variant="standard" 
+          value={inputValue} 
+          onChange={handleInputChange} 
+        />
+        <Button id='add-btn' onClick={handleAddToTodo}>ADD</Button>
+      </form>
       <ButtonsGroup />
 
       {/* To Do list */}
       <div>
         <ul className='mt-10'>
-          <li className='flex justify-between items-center w-70 text-purple-800 font-bold border-1 p-2 rounded-md shadow-md text-sm'>
-            <div>
-              <Checkbox  color="success" />
-              do my job
-            </div>
-            <IconButton aria-label="delete" size="small">
-              <DeleteIcon fontSize="small" sx={{ color: "purple" }} />
-            </IconButton>
-          </li>
+          {todoList.map((todo) => (
+            <li 
+              key={todo.id}
+              className='flex justify-between items-center w-70 text-purple-800 font-bold border-2 p-2 rounded-xl shadow-xl text-sm mb-3'
+            >
+              <div>
+                <Checkbox color="success" />
+                {todo.inputValue}
+              </div>
+              <IconButton aria-label="delete" size="small">
+                <DeleteIcon fontSize="small" sx={{ color: "purple" }} />
+              </IconButton>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
