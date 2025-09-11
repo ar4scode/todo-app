ceb6d71cb14c2a31985ded785957a703ca738534
Ar4sCode <ar4sazizi@gmail.com>
2025-09-11 20:27:36 +0330

    Redesign the todo items , input field and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './App.css';
-import ButtonsGroup from './components/ButtonsGroup';
+// import ButtonsGroup from './components/ButtonsGroup';
 import IconButton from "@mui/material/IconButton"; 
 import DeleteIcon from "@mui/icons-material/Delete"; 
 import Checkbox from "@mui/material/Checkbox"; 
@@ -53,39 +53,60 @@ function App() {
   return (
     <div className='flex flex-col items-center mb-70'>
       <form className='flex' onSubmit={(e) => e.preventDefault()}>
-        <TextField 
-          id="todo-value" 
-          label="To-Do" 
-          variant="standard" 
-          value={inputValue} 
-          onChange={handleInputChange} 
+        <TextField
+          id="todo-value"
+          label="To-Do"
+          variant="outlined"
+          value={inputValue}
+          onChange={handleInputChange}
+          sx={{
+            background: "#1b2432",
+            borderTopLeftRadius: "15px",
+            borderBottomLeftRadius: "15px",
+            "& .MuiOutlinedInput-root": {
+              "& fieldset": { border: "none" },             // remove default border
+              "&:hover fieldset": { border: "none" },       // remove on hover
+              "&.Mui-focused fieldset": { border: "none" }, // remove on focus
+            },
+            "& .MuiInputLabel-root" : {
+              color: "white"
+            },
+            "& .MuiInputLabel-root.Mui-focused": {
+              color: "white"
+            },
+            "& .MuiInputBase-input" : {
+              color: "#fff"
+            }
+          }}
         />
-        <Button id='add-btn' onClick={handleAddToTodo}>ADD</Button>
+        <Button id='add-btn' onClick={handleAddToTodo} sx={{background: "#1b2432" , color: "white", borderTopRightRadius: "15px", borderBottomRightRadius: "15px", marginLeft: "-3px"}}>ADD</Button>
       </form>
-      <ButtonsGroup />
+      {/* <ButtonsGroup /> */}
 
       {/* To Do list */}
-      <div className='bg-gray-500 mt-10 rounded-2xl'>
-        <p className='p-2'>number of tasks : {todoList.filter(todo => todo.isChecked).length} / {todoList.length}</p>
-        <ul className='mt-10'>
+      {todoList.length >= 1 && 
+        <div className='bg-gray-800 mt-10 rounded-2xl shadow-xl '>
+        <p className='px-5 mt-4 text-gray-400'>Number of tasks : {todoList.filter(todo => todo.isChecked).length} / {todoList.length}</p>
+        <ul className='mt-10 p-4'>
           {todoList.map((todo) => (
             <li 
               key={todo.id}
-              className='flex justify-between items-center w-70 text-purple-800 font-bold border-2 p-2 rounded-xl shadow-xl text-sm mb-3'
+              className='flex justify-between items-center w-70 text-gray-300 font-bold p-2 rounded-xl shadow-md text-sm mb-3 duration-75 hover:scale-110'
             >
               <div className='flex items-center'>
-                <Checkbox color="success" value={todo.isChecked} onChange={() => handleCheckboxChange(todo.id)}/>
+                <Checkbox color="success" sx={{color : "white"}} value={todo.isChecked} onChange={() => handleCheckboxChange(todo.id)}/>
                   <p style={{textDecoration : todo.isChecked ? "line-through" : "none"}}>
                     {todo.inputValue}
                   </p>
               </div>
               <IconButton aria-label="delete" size="small" onClick={() => handleDelete(todo.id)}>
-                <DeleteIcon fontSize="small" sx={{ color: "purple" }} />
+                <DeleteIcon fontSize="small" sx={{ color: "white" }} />
               </IconButton>
             </li>
           ))}
         </ul>
       </div>
+      }
     </div>
   );
 }
